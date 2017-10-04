7d45f1fea62e51eb8f54eef11d7dbc8c38f3f040
gvanhoy <gvanhoy@email.arizona.edu>
2017-10-03 22:48:04 -0700

    revised hf import

--- python/gen_watterson_taps.py
+++ python/gen_watterson_taps.py
@@ -1,5 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
 
 
 def gaussian_tap(delay_spread, doppler_spread, num_taps):
@@ -8,14 +7,6 @@ def gaussian_tap(delay_spread, doppler_spread, num_taps):
 
     taps = np.ndarray((num_taps,), dtype=np.float32)
     for tap_index in range(-num_taps/2, num_taps/2, 1):
-        #arg = np.pi*doppler_spread*tap_index/(samp_rate/2)
         arg = np.pi * doppler_spread * tap_index*2*delay_spread
         taps[tap_index + num_taps/2] = np.exp(-arg*arg)
-
-    plt.plot(taps)
-    plt.show()
     return taps
-
-
-if __name__ == '__main__':
-    gaussian_taps(1000, 1)
