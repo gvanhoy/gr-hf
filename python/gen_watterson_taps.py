70142db07abcc61d92f5fcc3e5ea25a1dacf539a
gvanhoy <basebzombie@gmail.com>
2017-10-14 13:04:29 -0700

    Added watterson tap generation

--- python/gen_watterson_taps.py
+++ python/gen_watterson_taps.py
@@ -1,20 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2017 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
 import numpy as np
 
 
-def gaussian_tap(delay_spread, doppler_spread, num_taps):
-    if num_taps % 2 != 0:
+def gen_taps(delay_spread, doppler_spread, num_taps):
+    if mod(num_taps, 2) != 0:
         print "Warning, number of taps is not even."
 
     taps = np.ndarray((num_taps,), dtype=np.float32)
     for tap_index in range(-num_taps/2, num_taps/2, 1):
         arg = np.pi * doppler_spread * tap_index*2*delay_spread
         taps[tap_index + num_taps/2] = np.exp(-arg*arg)
-
-    print '['
-    for tap in taps:
-        print str(tap) + ','
-    print ']'
-
-
-if __name__ == '__main__':
-    gaussian_tap(.001, 2, 100)
+    return taps
