a6135bbc8fc78de9a8b89fe289251fcdb7c6ccfe
gvanhoy <gvanhoy@email.arizona.edu>
2017-10-26 17:18:53 -0700

    11 taps

--- apps/watterson_equalization_error_rate.py
+++ apps/watterson_equalization_error_rate.py
@@ -35,45 +35,55 @@ class WattersonEqualization:
 
 
     def simulation(self):
-        tap1 = self.create_tap()
-        tap2 = self.create_tap()
-        snr_db_list = [1,2,3,4,5]
-        mse_list = []
+
+        snr_db_list = [1,1.5,2,2.5,3,3.5,4,4.5,5]
+        mse_avg_list = []
+        num_trials = 500000
         for x in snr_db_list:
+
             snr_db = 10*x
-            scale_factor = 1.0 / np.sqrt((tap1 * np.conj(tap1)) + (tap2 * np.conj(tap2)))
-            tap1 = scale_factor * tap1 * .5
-            tap2 = scale_factor * tap2 * .5
+            mse_avg = 0
+            for i in range(num_trials):
+                tap1 = self.create_tap()
+                tap2 = self.create_tap()
+                scale_factor = 1.0 / np.sqrt((tap1 * np.conj(tap1)) + (tap2 * np.conj(tap2)))
+                tap1 = scale_factor * tap1 * .5
+                tap2 = scale_factor * tap2 * .5
 
-            top_block = cma_watterson_experiment(snr_db, 4096, (tap1,tap2))
-            top_block.start()
-            top_block.wait()
-            self.symbols = top_block.blocks_vector_sink_x_0.data()
-            top_block.stop()
-            #lms_block = lms_watterson_experiment(snr_db,(tap1,tap2))
-            #lms_block.start()
-            #self.symbols_lms =
+                top_block = cma_watterson_experiment(snr_db, 4096, (tap1,tap2))
+                top_block.start()
+                top_block.wait()
+                self.symbols = top_block.blocks_vector_sink_x_0.data()
+                top_block.stop()
+                #lms_block = lms_watterson_experiment(snr_db,(tap1,tap2))
+                #lms_block.start()
+                #self.symbols_lms =
 
-            #plt.figure(1)
-            #plt.scatter(np.real(self.symbols)[0:100], np.imag(self.symbols)[0:100])
+                #plt.figure(1)
+                #plt.scatter(np.real(self.symbols)[0:100], np.imag(self.symbols)[0:100])
 
-            plt.figure(1)
-            plt.scatter(np.real(self.symbols)[3000:-1], np.imag(self.symbols)[3000:-1])
+                #plt.figure(1)
+                #plt.scatter(np.real(self.symbols)[3000:-1], np.imag(self.symbols)[3000:-1])
 
-            plt.title('Constellation Prior to Equalization - ' + str(snr_db) + ' dB')
-            plt.xlabel('In-Phase')
-            plt.ylabel('Quadrature')
-            plt.show()
+                #plt.title('Constellation Prior to Equalization - ' + str(snr_db) + ' dB')
+                #plt.xlabel('In-Phase')
+                #plt.ylabel('Quadrature')
+                #plt.show()
 
-            ## calculate the error rate
-            self.const_points = top_block.const.points()
+                ## calculate the error rate
+                self.const_points = top_block.const.points()
 
-            mse = self.error_check()
-            mse_list.append(mse)
+                mse = self.error_check()
+                mse_avg = mse_avg + mse/num_trials
+                print str(i)
+            mse_avg_list.append(mse_avg)
+            print i
 
-        mse_list = 10*np.log10(mse_list)
-        snr_plot = [10,20,30,40,50]
-        plt.plot(snr_plot, mse_list)
+
+        mse_avg_list = 10*np.log10(mse_avg_list)
+        #snr_plot = [10,20,30,40,50]
+        snr_plot = [10,15,20,25,30,35,40,45,50]
+        plt.plot(snr_plot, mse_avg_list)
         plt.xlabel('SNR (dB)')
         plt.ylabel('MSE (dB)')
         plt.title(' Mean Squared Error of CMA Equalizer')
